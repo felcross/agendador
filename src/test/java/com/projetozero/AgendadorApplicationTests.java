30febfa4e4b657c313b9bffe33d03c8310c4f366
Felipe <feljava32@gmail.com>
2026-01-29 16:12:41 -0300

    primeiro commit

---
+++ src/test/java/com/projetozero/AgendadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetozero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
