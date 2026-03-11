27b60bcce2fc273c0f4d54d4011f5248de05b246
Felipe <feljava32@gmail.com>
2026-03-11 17:15:10 -0300

    fix 4

---
+++ src/main/java/com/projetozero/infrastructure/exception/UnauthorizedException.java
@@ -0,0 +1,15 @@
+package com.projetozero.infrastructure.exception;
+
+
+import org.springframework.security.core.AuthenticationException;
+
+public class UnauthorizedException extends AuthenticationException {
+
+    public UnauthorizedException(String mensagem){
+        super(mensagem);
+    }
+
+    public UnauthorizedException(String mensagem, Throwable throwable){
+        super(mensagem, throwable);
+    }
+}
--- src/main/java/com/projetozero/infrastructure/security/UserDetailsServiceImpl.java
+++ src/main/java/com/projetozero/infrastructure/security/UserDetailsServiceImpl.java
@@ -2,30 +2,27 @@ package com.projetozero.infrastructure.security;
 
 
 
-import com.projetozero.controller.dto.UsuarioDTO;
+import com.projetozero.controller.dto.UsuarioDTORecord;
 import com.projetozero.infrastructure.client.UsuarioClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserDetailsServiceImpl  {
+public class UserDetailsServiceImpl {
 
     @Autowired
-     private UsuarioClient usuarioClient;
+    private UsuarioClient client;
 
+    public UserDetails carregaDadosUsuario(String email, String token){
 
-
-    public UserDetails carregaDadosUsuario(String email, String token) {
-
-        UsuarioDTO client = usuarioClient.buscarUsuarioPorEmail(email,token);
-
-        return org.springframework.security.core.userdetails.User
-                .withUsername(client.getEmail()) // Define o nome de usuário como o e-mail
-                .password(client.getSenha()) // Define a senha do usuário
-                .build(); // Constrói o objeto User
-
+        UsuarioDTORecord usuarioDTO = client.buscaUsuarioPorEmail(email, token);
+        return User
+                .withUsername(usuarioDTO.email())
+                .password(usuarioDTO.senha())
+                .build();
     }
+
+
 }
